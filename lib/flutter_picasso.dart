849c6756a371dd18a3da91b1494862a599940d7c
shahpalsha <shahpalsha@gmail.com>
2026-02-13 16:29:16 +0530

    .

--- lib/flutter_picasso.dart
+++
@@ -1,4 +0,0 @@
-library flutter_picasso;
-
-export 'src/widgets/picasso_canvas.dart';
-export 'src/controller/picasso_controller.dart';
--- lib/src/models/draw_point.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-class DrawPoint {
-  final Offset offset;
-
-  DrawPoint(this.offset);
-}
